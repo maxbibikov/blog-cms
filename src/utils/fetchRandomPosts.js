2b45cb90abcf3975d79141dbeeacca3d672eb0bb
Maksym Bibikov <bibikovmax@gmail.com>
2020-04-16 18:23:40 +0300

    ⚙️  change news api to https

--- src/utils/fetchRandomPosts.js
+++ src/utils/fetchRandomPosts.js
@@ -3,7 +3,7 @@ export const fetchRandomPosts = () => {
   const randomNum = Math.round(Math.random() * (keywords.length - 1));
 
   return fetch(
-    `http://newsapi.org/v2/top-headlines?sources=${keywords[randomNum]}&pageSize=20&sortBy=publishedAt&apiKey=a4efcebcbcbc437ba9d11d50605d827d`
+    `https://newsapi.org/v2/top-headlines?sources=${keywords[randomNum]}&pageSize=20&sortBy=publishedAt&apiKey=a4efcebcbcbc437ba9d11d50605d827d`
   )
     .then(response => {
       if (response.ok) {
